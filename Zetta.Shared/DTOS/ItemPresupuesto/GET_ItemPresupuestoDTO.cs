d49599308b1de4670079f85d4106a67cbe760f33
leonardo contreras <leo8292014@hotmail.com>
2025-10-06 10:03:20 -0300

    agregado de DTOs faltantes

---
+++ Zetta.Shared/DTOS/ItemPresupuesto/GET_ItemPresupuestoDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zetta.BD.DATA.ENTITY;
+
+namespace Zetta.Shared.DTOS.ItemPresupuesto
+{
+    public class GET_ItemPresupuestoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public Rubro Rubro { get; set; }
+        public string? Medida { get; set; }
+        public string? Material { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Fabricante { get; set; }
+        public string? Marca { get; set; }
+        public DateTime? FechActuPrecio { get; set; }
+    }
+}
---
+++ Zetta.Shared/DTOS/Presupuesto/PUT_PresupuestoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zetta.BD.DATA.ENTITY;
+
+namespace Zetta.Shared.DTOS.Presupuesto
+{
+    public class PUT_PresupuestoDTO
+    {
+        public int Id { get; set; }
+        public Rubro Rubro { get; set; }
+        public bool Aceptado { get; set; }
+        public string? Observacion { get; set; }
+        public decimal? ManodeObra { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalP { get; set; }
+        public string TiempoAproxObra { get; set; }
+        public string ValidacionDias { get; set; }
+        public OpcionDePago OpcionDePago { get; set; }
+        public int ClienteId { get; set; }
+        public List<PUT_PresItemDetalleDTO>? ItemsDetalle { get; set; }
+    }
+}
